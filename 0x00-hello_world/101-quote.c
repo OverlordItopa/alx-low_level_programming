11cdad512351ea66117ecab6dfb4c01c3b34997d
Victor <oseevictor@gmail.com>
2023-02-21 14:57:55 -0800

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", c);
+	return (1);
+}
