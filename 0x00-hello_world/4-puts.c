0fa09d2c95f96886abcde38675b3eae3de9e3ec2
eternal <oseevictor@gmail.com>
2023-05-18 15:19:12 -0700

    all ther est

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - Prints out a string to stdout.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
